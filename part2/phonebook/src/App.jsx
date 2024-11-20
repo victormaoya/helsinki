140875b8c65734b3d70acb5c5dcfcb2b749a6ea4
Victor Maoya <victormaoya@gmail.com>
2024-11-20 16:40:21 +0300

    the Effect Hook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Filter  from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number : '12-43-234345', id: 3},
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-    { name: 'Elon Musk', number: '0725-522-488', id: 5 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [query, setQuery] = useState('')
   const [filteredPersons, setFilteredPersons] = useState([])
+
+  
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const persons = response.data
+        setPersons(persons)
+      })
+  }, [])
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
