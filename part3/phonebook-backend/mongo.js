ac5b47c6f432cc33ff385c3994f7d89d30a1843c
Victor Maoya <victormaoya@gmail.com>
2024-11-28 19:59:04 +0300

    Used mongoose to add data to the database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://helios:${password}@cassavetes.n7xpk.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cassavetes`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
