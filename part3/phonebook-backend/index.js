45b254efb42c3c64d37aecaa8c56c54cc6205803
Victor Maoya <victormaoya@gmail.com>
2024-12-01 15:05:28 +0300

    backend nsync with frontend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,9 +8,19 @@ const morgan = require('morgan')
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
@@ -20,19 +30,27 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
   })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  response.send(
-    `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date().toString()}</p>
-    `
-  )
+  Person.find({}).then(persons => {
+    response.send(
+      `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date().toString()}</p>
+      `
+    )    
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -52,12 +70,30 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(p => p.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(deletedPerson => {
+      response.json(deletedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
