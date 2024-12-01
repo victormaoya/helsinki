b15f3a4b6bcfd1a022ca5d46012b8693497e76bb
Victor Maoya <victormaoya@gmail.com>
2024-12-01 10:16:54 +0300

    Interaction with MongoDB

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,54 +1,29 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
+const Person = require('./models/person')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const app = express()
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -63,28 +38,18 @@ app.get('/info', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name is missing'
-    })
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: 'number is missing'
-    })
-  } else if (persons.find(p => p.name === body.name)) {
-    return response.status(409).json({
-      error: 'name already exists'
-    })
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const person = {
-    id: Math.floor(Math.random() * 100),
+  const person = new Person({
     name: body.name,
-    number: body.number,
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -95,7 +60,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
