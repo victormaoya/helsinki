94557902e2a1c2983ec945fa8c733443d1474172
Victor Maoya <victormaoya@gmail.com>
2024-12-03 14:49:19 +0300

    moved all app logic to app.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,20 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
